1debacda0417dc8dec87d20eb1229f8c6c7d80d8
Santiago Alvarez <santiago.alvarez3434@gmail.com>
2023-11-06 16:30:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
+//Simulador basico de tienda, en este simulador agregamos un producto o servicio ingresando los precios a pagar y asi vamos acumulando, para luego ver el precio total.
+
+// Defino variable
 let total = 0;
 
-
+// calcular valor total 
 function calcularTotal(precio) {
   total += precio;
 }
 
-
+// Bucle para agregar productos o servicios
 do {
-  let precioProducto = parseFloat(prompt("Ingresa el precio del producto o servicio (o escribe 'No' para finalizar):"));
+  let precioProducto = prompt("Ingresa el precio del producto o servicio (o escribe 'No' para finalizar):");
 
-  if (!isNaN(precioProducto)) {
+  if(precioProducto === null){
+    alert("Gracias por visitarnos")
+    break;
+  }
+
+  if (!isNaN(parseFloat(precioProducto))) {
+    precioProducto = parseFloat(precioProducto); // Convertimos a número
     calcularTotal(precioProducto);
     alert("Valor acumulado: $" + total);
   } else if (precioProducto.toLowerCase() === "no") {
-    
+    // si el usuario escribe no, salimos del bucle
     break;
   } else {
     alert("Ingresa un número válido o escribe 'No' para finalizar.");
   }
 } while (true);
 
+// Mostrar el valor total acumulado
 
-alert("El valor total es: $" + total);
+if (total > 0){
+  alert("El valor total es: $" + total);
+}
+
 
